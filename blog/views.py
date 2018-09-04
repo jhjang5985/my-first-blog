857e7f4b31ddc40e5047f3edda4fbe02cb2b13c7
jaehyeok <jhjang5985@gmail.com>
2018-09-04 22:16:01 +0900

    big update!

--- blog/views.py
+++ blog/views.py
@@ -3,49 +3,63 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
-def post_list(requset):
+def post_list(request):
     qs = Post.objects.all()
     qs = qs.filter(published_date__lte=timezone.now())
     qs = qs.order_by('published_date')
 
-    return render(requset, 'blog/post_list.html', {
+    return render(request, 'blog/post_list.html', {
         'post_list' : qs,
     })
 
-def post_detail(requset, pk):
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(requset, 'blog/post_detail.html', {
+    return render(request, 'blog/post_detail.html', {
         'post' : post
     })
 
-def post_new(requset):
-    if requset.method == 'POST':
-        form = PostForm(requset.POST, requset.FILES)
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
         form = PostForm()
 
-    return render(requset, 'blog/post_new.html', {
+    return render(request, 'blog/post_new.html', {
         'form' : form,
     })
 
-def post_edit(requset, pk):
+def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
-    if requset.method == 'POST':
-        form = PostForm(requset.POST, requset.FILES, instance=post)
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
         form = PostForm(instance=post)
 
-    return render(requset, 'blog/post_edit.html', {
+    return render(request, 'blog/post_edit.html', {
         'form' : form,
     })
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
