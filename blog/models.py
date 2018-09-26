6d4ff944848756805dc5af10a4e9c3b0b2a10bd6
jaehyeok <jhjang5985@gmail.com>
2018-09-26 14:38:08 +0900

    none

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=100)
-    text = models.CharField(max_length=100)
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
